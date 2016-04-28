a3c08696529b612d32b2c3d39372838ab696940a
Vihan Chaudhry <vihanchaudhry@gmail.com>
2016-04-28 02:11:01 +0000

    Formatting

--- db/schema.rb
+++ db/schema.rb
@@ -14,11 +14,11 @@
 ActiveRecord::Schema.define(version: 20160427233824) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "password_digest"
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
